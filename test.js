ffd0299253043d16de3abc00940cd341ad2f2daa
Daniel Gao <daniel.gao6205@gmail.com>
2024-09-15 20:52:57 -0400

    Revert "string true"
    This reverts commit 33f453923d8c70c4b008fc5f92077458668f730a.

--- test.js
+++ test.js
@@ -15,8 +15,11 @@ function capitalizeFirstLetter(str) {
     if (!Array.isArray(arr)) {
       throw new TypeError('Input must be an array');
     }
-    // Return true if the array contains only unique elements
-    return true; 
+    if (true) {
+      return true;
+    } else {
+      return arr.length === new Set(arr).size && true;
+    }
   }
   
   
